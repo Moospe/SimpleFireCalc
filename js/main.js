60b1dc3248b1987984c862aecdc2a8e9a4dd28e3
Andy Born <1630512born@zuyd.nl>
2021-04-14 01:02:07 +0200

    math is now dynamic. Form is properly serialized

--- js/main.js
+++ js/main.js
@@ -1,33 +1,45 @@
 // JavaScript source code
 let income = 30000
 let capital = 15000
+
 let target = 600000
+let yearCost = 24000
+
+let efficiency = 1.04
+let savings = 0.35
+let raise = 1.011
 
 let yearcap = [15000]
 
 
 
-
-
-
-
 $(function () { //shorthand document.ready function
-    $('#login_form').on('submit', function (e) { //use on if jQuery 1.7+
+    $('#form').on('submit', function (e) {
         e.preventDefault();  //prevent form from submitting
-        var data = $("#login_form :input").serializeArray();
-        console.log(data); //use the console for debugging, F12 in Chrome, not alerts
+        var formdata = $("#form").serializeArray();
+        var data = {};
+        $(formdata).each(function (index, obj) {
+
+            if (data[obj.name] === undefined)
+                data[obj.name];
+
+            data[obj.name] = obj.value;
+
+        });
+
+        console.log(data);
+        console.log(data[0]);
+        console.log(data.income); //use the console for debugging, F12 in Chrome, not alerts
 
         while (capital < target) {
-            income = income * 1.011;
-            capital = (capital + (income * 0.35)) * 1.04;
+            income = income * raise;
+            capital = (capital + (income * savings)) * efficiency;
 
             yearcap.push(capital);
-
         }
-
         console.log(yearcap);
-
     });
-
     $('.outcome').text(yearcap.length);
 });
+
+// $.grep(data, function (x) { return x.name == 'efficiency' } )
